7b8cb5a4ede4d33c6b0531243e68a4de8bed9334
Melania <melaniaogalla@gmail.com>
2021-12-22 16:24:38 +0100

    Liskov Substitution closes #5

---
+++ PrincipiosSolid/LiskovSubstitution/Correcto/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.LiskovSubstitution.Correcto
+{
+    public class Main
+    {
+        // ...
+        public static void main(string[] args)
+        {
+            Coche[] arrayCoches = {
+        new Renault(),
+        new Audi(),
+        new Mercedes()
+        };
+            imprimirNumAsientos(arrayCoches);
+            imprimirPrecioMedio(arrayCoches);
+        }
+
+        public static void imprimirNumAsientos(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.numAsientos());
+            }
+        }
+
+        public static void imprimirPrecioMedio(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.precioMedioCoche());
+            }
+        }
+
+
+    }
+}
+
