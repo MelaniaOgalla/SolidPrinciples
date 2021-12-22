c3684c35071c6bd437c674d318b3fcbe08384732
Melania <melaniaogalla@gmail.com>
2021-12-22 16:23:50 +0100

    Liskov Substitution References: #5

---
+++ PrincipiosSolid/LiskovSubstitution/Incorrecto/Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.LiskovSubstitution.Incorrecto
+{
+    public class Main
+    {
+        // ...
+        public static void main(string[] args)
+        {
+        Coche[] arrayCoches = {
+        new Renault(),
+        new Audi(),
+        new Mercedes()
+        };
+            imprimirNumAsientos(arrayCoches);
+        }   
+
+        public static void imprimirNumAsientos(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                if (coche.GetType() == typeof(Renault))
+
+                    Console.WriteLine("Num asientos 4");
+
+                if (coche.GetType() == typeof(Audi))
+
+                    Console.WriteLine("Num asientos 5");
+
+                if (coche.GetType() == typeof(Mercedes))
+
+                    Console.WriteLine("Num asientos 4");
+
+            }
+        }
+}
+}
---
+++ PrincipiosSolid/LiskovSubstitution/Incorrecto/Renault.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.LiskovSubstitution.Incorrecto
+{
+    public class Renault : Coche
+    {
+        int numAsientosRenault(Coche coche)
+        {
+            return 5;
+        }
+    }
+}
