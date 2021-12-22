b6b5f53ac4a1de1556179a3b224d345ca5b0ba05
Melania <melaniaogalla@gmail.com>
2021-12-22 10:30:57 +0100

    SECOND COMMIT

---
+++ PrincipiosSolid/InterfaceSegregation/Correcto/Coche.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrincipiosSolid
+{
+    public class Coche
+    {
+        String Marca;
+
+        Coche(string marca)
+        {
+            this.Marca = marca;
+        }
+    }
+
+}
---
+++ PrincipiosSolid/InterfaceSegregation/Incorrecto/Coche.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrincipiosSolid
+{
+    public class Coches
+    {
+        String Marca;
+
+        Coches(string marca)
+        {
+            this.Marca = marca;
+        }
+
+        string getMarcaCoche()
+        {
+            return Marca;
+        }
+
+        void guardarCocheDB(Coche coche)
+        {
+
+        }
+    }
+}
