2aed3e81f12ec5132130c5d4dbac0578c58ddac3
Melania <melaniaogalla@gmail.com>
2021-12-22 12:54:50 +0100

    Add Open Close Principal Example closes #3

---
+++ PrincipiosSolid/OpenClose/Correcto/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.OpenClose.Correcto
+{
+    public class Main
+    {
+        public static void main (String[] args)
+        {
+            Coche[] arrayCoches =
+            {
+                new Audi(),
+
+            };
+            imprimirPrecioMedioCoche(arrayCoches);
+        }
+
+        public static void imprimirPrecioMedioCoche(Coche [] arrayCoches)
+        {
+            foreach(Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.precioMedioCoche());
+            }
+        }
+    }
+}
---
+++ PrincipiosSolid/OpenClose/Incorrecto/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.OpenClose.Incorrecto
+{
+    public class Main
+    {
+        public static void main(string[] args)
+        {
+            Coche[] arrayCoches = {
+            new Coche("Renault"),
+            new Coche("Audi")
+            };
+            imprimirPrecioMedioCoche(arrayCoches);
+        }
+
+        public static void imprimirPrecioMedioCoche(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                if (coche.marca.Equals("Renault")) Console.WriteLine(18000);
+                if (coche.marca.Equals("Audi")) Console.WriteLine(25000);
+            }
+        }
+    }
+}
